42911dcb1921e62abae245afa8eae8de7f9fc7cd
Choi702 <jaeychoi1@gmail.com>
2021-03-25 20:58:00 -0700

    finish rebuild of lab33 still need to finish

---
+++ auth_blog/views.py
@@ -0,0 +1,16 @@
+from auth_blog.permissions import IsOwnerOrReadOnly
+from rest_framework import generics
+from rest_framework.permissions import IsAuthenticatedOrReadOnly
+from .models import auth_blog
+from .serializers import auth_blogSerializer
+
+
+class auth_blogList(generics.ListCreateAPIView):
+    permission_classes = (IsAuthenticatedOrReadOnly,)
+    queryset = auth_blog.objects.all()
+    serializer_class = auth_blogSerializer
+
+class auth_blogDetail(generics.RetrieveUpdateDestroyAPIView):
+    permission_classes = (IsOwnerOrReadOnly,)
+    queryset = auth_blog.objects.all()
+    serializer_class = auth_blogSerializer
